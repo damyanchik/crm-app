8cab50171816852b5a7cbf0131af81ef1874089f
damyanchik <d4mian.ndz@gmail.com>
2023-11-09 13:42:33 +0100

    employee block event

---
+++ app/Events/UserLogging.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Queue\ShouldBroadcast;
+use Illuminate\Support\Facades\Auth;
+
+class UserLogging
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __invoke()
+    {
+        if (Auth::check() && Auth::user()->block == 1) {
+            Auth::logout();
+        }
+    }
+}
